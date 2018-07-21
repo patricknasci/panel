a2613694eff680eb82b3c3de1dbbfdc816ace05b
patricknasci <patricknasci@hotmail.com>
2018-07-21 17:18:21 -0300

    Create passwords.php

---
+++ resources/lang/pt/passwords.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+    'password' => 'Senha tem de ser maior de 6 caracteres e corresponder com a senha de confirmação.',
+    'reset' => 'Sua senha foi redefinida com sucesso!',
+    'sent' => 'Nós enviamos-lhe um email com link de redefinição de senha!',
+    'token' => 'Este token de redefinição é invalido.',
+    'user' => "Não foi possivel encontrar usuario com esse endereço de e-mail.",
+];
